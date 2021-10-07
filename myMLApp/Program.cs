2cc04d57d6d4545e9b32d75746403ebd8207eaa7
Imamul Hassan <imamul.hassan@bjitgroup.com>
2021-10-07 09:32:53 +0600

    Add project files.

---
+++ myMLApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyMLApp;
+using System;
+
+namespace myMLApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = "This restaurant was wonderful."
+            };
+
+            // Load model and predict output of sample data
+            var result = SentimentModel.Predict(sampleData);
+
+            // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+            string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+        }
+    }
+}
